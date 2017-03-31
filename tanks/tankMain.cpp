9b711c63186ed8759906ddca6b6c6b098ec6dd85
Pat Myron <pmyron09@gmail.com>
2017-03-30 21:21:01 -0400

    making functions void when return types aren't used

--- tanks/tankMain.cpp
+++ tanks/tankMain.cpp
@@ -10,8 +10,8 @@
 
 using namespace std;
 
-apvector<int> drawLandscape(window &, color skycolor, color groundcolor); // creates landscape. returns height of ground at every x-value. only called once
-bool getInput(window &, color skycolor, tank &left, tank &right, bool &leftTurn); // gets input for 2 parameters. returns true when enter key (shoot) is pressed
+void drawLandscape(window &, color skycolor, color groundcolor); // creates landscape. only called once
+void getInput(window &, color skycolor, tank &left, tank &right, bool &leftTurn); // gets input for 2 parameters. returns when enter key (shoot) is pressed
 color bullet(window &, color skycolor, tank &left, tank &right, bool &leftTurn); // goes through bullets path. returns color of what is hit.
 
 void setBrushAndPenColor(window &w, color brushAndPenColor) {
@@ -29,7 +29,7 @@ void main() {
     color tankcolor2 = color(40, 40, 40); // left parentheses
     window w(1250, 900);
     w.SetBuffering(true);
-    drawLandscape(w, skycolor, groundcolor); // only called once. can return array of ground heights[]
+    drawLandscape(w, skycolor, groundcolor); // only called once
     tank left(w, skycolor, groundcolor, tankcolor1, 1);  // angle initialized to 45
     tank right(w, skycolor, groundcolor, tankcolor2, 0); // speed initialized to 25
     left.drawTank(w);
@@ -56,22 +56,19 @@ void main() {
     w.UpdateBuffer();
 }
 
-apvector<int> drawLandscape(window &w, color skycolor, color groundcolor) { // creates landscape. returns height of ground at every x-value. only called once
+void drawLandscape(window &w, color skycolor, color groundcolor) { // creates landscape. only called once
     setBrushAndPenColor(w, skycolor);
     w.DrawRectangle(0, 0, w.GetWidth(), w.GetHeight(), FILLED);
     RandGen r;
-    apvector<int> heights(w.GetWidth()); // height of ground at every x-value
     int ystart = r.RandInt((double) w.GetHeight() * 0.666, (double) w.GetHeight() * 0.75); // semi random height of ground near bottom of screen
     setBrushAndPenColor(w, groundcolor);
-    for (double x = 0; x < w.GetWidth(); x++) { // filling heights[]
+    for (double x = 0; x < w.GetWidth(); x++) {
         int yval = ystart + sin((double) (x / 60)) * 15; // ground will be sin curve
         w.DrawLine(x, yval, x, w.GetHeight());    // draws line from ground level to bottom of screen
-        heights[x] = yval;
     }
-    return heights;
 }
 
-bool getInput(window &w, color skycolor, tank &left, tank &right, bool &leftTurn) { // gets input for 2 parameters. returns true when enter key (shoot) is pressed
+void getInput(window &w, color skycolor, tank &left, tank &right, bool &leftTurn) { // gets input for 2 parameters. returns when enter key (shoot) is pressed
     char k = '#';  // for key, set to a known, but useless value
     int angle;
     int velocity;
@@ -130,7 +127,6 @@ bool getInput(window &w, color skycolor, tank &left, tank &right, bool &leftTurn
         }
         w.UpdateBuffer();
     }
-    if (k == 13) return true;
 }
 
 color bullet(window &w, color skycolor, tank &left, tank &right, bool &leftTurn) { // goes through bullets path. returns color of what is hit.
